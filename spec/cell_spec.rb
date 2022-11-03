f739f3d41d0431a068bebdd91187c7b5b0bd3ac6
Jordan Skomal <jskomal@gmail.com>
2022-11-02 19:29:13 -0600

    Begin Tests for cell

---
+++ spec/cell_spec.rb
@@ -0,0 +1,30 @@
+require './lib/cell'
+require './lib/ship'
+
+RSpec.describe Cell do
+  before do
+    @cell = Cell.new('B4')
+  end
+
+  it 'exists' do
+    expect(@cell).to be_instance_of(Cell)
+  end
+
+  it 'has a coordinate' do
+    expect(@cell.coordinate).to eq('B4')
+  end
+
+  it 'may contain a ship' do
+    expect(@cell.ship).to eq(nil)
+  end
+
+  it 'should be empty by default' do
+    expect(@cell.empty?).to eq(true)
+  end
+
+  it 'should be able to place a ship in a cell' do
+    @cell.place_ship
+    expect(@cell.ship).to be_instance_of(Ship)
+    expect(@cell.empty?).to eq(false)
+  end
+end
