a8efc97426b6140ae4ffb954ddedd8af19449edc
Jordan Skomal <jskomal@gmail.com>
2022-11-02 19:29:24 -0600

    Pass early tests for cell

---
+++ lib/cell.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Cell
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def empty?
+    @ship.nil?
+  end
+end
